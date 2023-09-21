a5db7aae4797dbfa2add0c9883a136e8053c186e
DESKTOP-J1BRRPQ\XP <andreslopezch1996@gmail.com>
2023-09-21 14:44:10 -0600

    Cambios realizados en clase (Falta terminar codigo y agregarlo en la jerarquia)

---
+++ Assets/SCRIPTS/LOBBY/SlotPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class SlotPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
